61166ba6004d74ac3fefb33d88840d02c18a8512
PhatBee <vinhphatst1235@gmail.com>
2025-09-03 09:06:02 +0700

    API: get Products Example Data: seed

--- ExpressJS01/src/routes/api.js
+++ ExpressJS01/src/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const auth = require('../middleware/auth');
 const delay = require('../middleware/delay');
 const { createUser, handleLogin, getUser, getAccount } = require('../controllers/userController');
+const { listProducts } = require('../controllers/productController');
 
 const apiRoutes = express.Router();
 
@@ -15,5 +16,6 @@ apiRoutes.post("/register", createUser);
 apiRoutes.post("/login", handleLogin);
 apiRoutes.get("/user", getUser);
 apiRoutes.get("/account", delay, getAccount);
+apiRoutes.get("/products", listProducts);
 
 module.exports = apiRoutes;
